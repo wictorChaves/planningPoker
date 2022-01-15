91cbc48d4a1c5347f0903f921e4d540b19db8bee
Wictor Chaves <wictor@hotmail.com.br>
2022-01-15 20:19:19 -0300

    Publish script

---
+++ publish.cmd
@@ -0,0 +1,15 @@
+call npm run build-prod
+git checkout gh-pages
+del *.txt /Q
+del *.ico /Q
+del *.html /Q
+del *.js /Q
+del *.css /Q
+rmdir "assets" /S /Q
+xcopy /s dist\planningPoker .\
+git rm -r --cached .
+git add -A
+git commit -am "Publish"
+git push
+git checkout master
+pause
