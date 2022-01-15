c49fdd1c583085c617aca80dedc6852ef3a82457
Wictor Chaves <wictor@hotmail.com.br>
2022-01-15 20:34:16 -0300

    Novos scripts para publicação

---
+++ publish-continue.cmd
@@ -0,0 +1,15 @@
+cd ..
+git checkout gh-pages
+del *.txt /Q
+del *.ico /Q
+del *.html /Q
+del *.js /Q
+del *.css /Q
+rmdir "assets" /S /Q
+xcopy /s dist\planningPoker .\
+git rm -r --cached .
+git add -A
+git commit -am "Publish"
+git push
+git checkout master
+pause
--- publish.cmd
+++ publish.cmd
@@ -1,16 +1,4 @@
 call npm run build-prod
-git checkout gh-pages publish.cmd
-git checkout gh-pages
-del *.txt /Q
-del *.ico /Q
-del *.html /Q
-del *.js /Q
-del *.css /Q
-rmdir "assets" /S /Q
-xcopy /s dist\planningPoker .\
-git rm -r --cached .
-git add -A
-git commit -am "Publish"
-git push
-git checkout master
-pause
+copy publish-continue.cmd dist\publish-continue.cmd
+cd dist
+start "" publish-continue.cmd
